599b0136eeaf4b9c8378f7392bad07fd008b35a4
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2025-11-16 12:19:38 +0000

    s

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-# print the required sentence using slices (edges)
-print(str[33:50] + " " + str[50:61] + " " + str[62:74])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[40:67] + str[107:112] + str[0:6])

24fcd07ec4bcc4920c73ee161f46e631c42eb1cc
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2025-11-22 12:04:13 +0000

    j

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (TypeError, IndexError):
-            if isinstance(my_list[i], (int, float)):
-                continue
+        except (TypeError, ValueError):
+            continue
+        except IndexError:
             break
     print()
     return count

0574e22d6242db2f8cb38cb7ccd8519e429eea96
shukufa <shukufa@kali.kali>
2025-11-28 16:24:06 -0500

    python-serialization

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import csv
+import json
+
+def convert_csv_to_json(csv_filename):
+    """
+    Convert CSV data to JSON format and save it to data.json.
+    
+    Args:
+        csv_filename (str): The CSV input file name.
+    
+    Returns:
+        bool: True if conversion is successful, False otherwise.
+    """
+    try:
+        with open(csv_filename, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            data = [row for row in reader]
+
+        with open("data.json", "w", encoding="utf-8") as jsonfile:
+            json.dump(data, jsonfile, indent=4)
+
+        return True
+    except Exception:
+        return False

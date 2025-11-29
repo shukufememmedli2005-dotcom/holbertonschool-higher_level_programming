274e67fdb86859d82bb7a6a4f9b77891e192807b
shukufa <shukufa@kali.kali>
2025-11-29 10:46:44 -0500

    SQL_Introduction

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,8 @@
 -- List the number of records per score in second_table
 -- The result should display 'score' and 'number'
 -- Sorted by number of records descending
-SELECT score COUNT(*) AS numbers FROM second_table GROUP BY score ORDER BY number DESC;
+
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;

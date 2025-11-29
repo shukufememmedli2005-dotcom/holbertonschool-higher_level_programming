422c02350ef30698307300fbb92f960b8f107950
shukufa <shukufa@kali.kali>
2025-11-29 12:30:37 -0500

    SQL_more_queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,15 +1,7 @@
--- Check and create the users if they do not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Optional: grant some privileges if you want them to have any
--- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- List privileges for user_0d_1 (only if exists)
-SELECT CONCAT('Grants for user_0d_1@localhost:') AS info;
+-- Attempt to show privileges for user_0d_1 on localhost
+-- Since the user does not exist, MySQL will return ERROR 1141
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List privileges for user_0d_2 (only if exists)
-SELECT CONCAT('Grants for user_0d_2@localhost:') AS info;
+-- Attempt to show privileges for user_0d_2 on localhost
+-- Since the user does not exist, MySQL will return ERROR 1141
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

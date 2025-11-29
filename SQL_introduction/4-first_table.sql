05eba6d461031a6d5bc69725167d3a60efd78eae
shukufa <shukufa@kali.kali>
2025-11-29 09:21:06 -0500

    SQL_Introduction

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- Create first_table if it does not exist
-CREATE DTABASE IF NOT EXISTS hbtn_test_db_4;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_4;
 USE hbtn_test_db_4;
 CREATE TABLE IF NOT EXISTS first_table(
-id INT
+id INT,
 name VARCHAR(256)
 );

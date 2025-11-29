474a336448326fd41f7860a45aa80e0921573475
shukufa <shukufa@kali.kali>
2025-11-29 12:32:39 -0500

    SQL_more_queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,9 @@
--- Create the user if it does not exist
+-- Create the user user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges on the server to the user
+-- Grant all privileges on all databases and tables to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Apply the changes immediately
-FLUSH PRIVILEGES;x
+FLUSH PRIVILEGES;
+

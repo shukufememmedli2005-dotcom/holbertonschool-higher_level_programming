932b0ef8b3bd93bfbb98a6109f90ec96f38a85c8
shukufa <shukufa@kali.kali>
2025-11-29 10:30:38 -0500

    SQL_Introduction

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,4 @@
 -- Compute the average of all scores in second_table
 -- The result column name will be 'average'
-SELECT AVG(score) AS avreage FROM second_table;
+SELECT AVG(score) AS average
+FROM second_table;

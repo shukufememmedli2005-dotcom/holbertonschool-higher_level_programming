7ca233fd7399a0b69289cdf17ce4b3324b9559f5
shukufa <shukufa@kali.kali>
2025-11-29 10:16:00 -0500

    SQL_Introduction

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List all records from second_table with score >= 10
+-- Show only score and name columns, ordered by score descending
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;

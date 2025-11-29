b86bddc422eb22bb992967df87dacc9233d85a26
shukufa <shukufa@kali.kali>
2025-11-29 12:42:34 -0500

    SQL_more_queries

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,15 @@
+-- List all shows with their genres
+-- Display: tv_shows.title - tv_genres.name
+-- If a show has no genre, display NULL
+-- Sort by tv_shows.title ASC and tv_genres.name ASC
+SELECT 
+    tv_shows.title,
+    tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY 
+    tv_shows.title ASC,
+    tv_genres.name ASC;

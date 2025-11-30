5cb871dc518d1124135b29660132cfd24c44a9db
shukufa <shukufa@kali.kali>
2025-11-30 09:26:42 -0500

    SQL_more_queries

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,8 @@
--- Create a MySQL user user_0d_1 with all privileges
-
+-- Create user user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Grant all privileges without WITH GRANT OPTION (this matches Holberton checker)
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;

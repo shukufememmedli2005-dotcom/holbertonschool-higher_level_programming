bf5bc95b240fd760777d01d7a5c2d201338ba4d5
shukufa <shukufe.memmedli2005@gmail.com>
2025-11-30 10:07:08 -0500

    python_input_output

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev = triangle[-1]
+        row = [1]
+
+        for j in range(1, i):
+            row.append(prev[j - 1] + prev[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle

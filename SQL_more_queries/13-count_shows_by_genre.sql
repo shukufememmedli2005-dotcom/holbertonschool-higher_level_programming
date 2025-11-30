ee78464329dd49060fcb02ffb98818fd5ed5c4b8
shukufa <shukufa@kali.kali>
2025-11-30 09:36:44 -0500

    SQL_more_queries

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Count the number of shows per genre
+SELECT g.name AS genre, COUNT(tg.show_id) AS number_of_shows
+FROM genres g
+JOIN tv_show_genres tg ON g.id = tg.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;

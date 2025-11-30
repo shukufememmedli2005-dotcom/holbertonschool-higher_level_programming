623cdc393cf41c784a9d51f94924c75c57f884d8
shukufa <shukufa@kali.kali>
2025-11-30 09:40:36 -0500

    SQL_more_queries

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,13 +1,8 @@
--- List all shows with the genre Comedy
--- Display: tv_shows.title
--- Sort by tv_shows.title ASC
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.genre_id = (
-    SELECT id
-    FROM tv_genres
-    WHERE name = 'Comedy'
-)
-ORDER BY tv_shows.title ASC;
+-- List all Comedy shows
+
+SELECT S.title
+FROM tv_shows S
+JOIN tv_show_genres SG ON S.id = SG.show_id
+JOIN tv_genres G ON SG.genre_id = G.id
+WHERE G.name = 'Comedy'
+ORDER BY S.title ASC;

75c8a9304d2c6aee147d928c22f55470e82c18ee
shukufa <shukufa@kali.kali>
2025-11-30 09:30:42 -0500

    SQL_more_queries

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,5 @@
--- List all cities with their state names in hbtn_0d_usa
--- Display: cities.id - cities.name - states.name
--- Sort by cities.id in ascending order
-SELECT 
-    id,
-    name,
-    (SELECT name FROM states WHERE states.id = cities.state_id) AS state_name
+-- List all cities with their corresponding state name
+SELECT cities.id, cities.name, states.name
 FROM cities
-ORDER BY id ASC;
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;

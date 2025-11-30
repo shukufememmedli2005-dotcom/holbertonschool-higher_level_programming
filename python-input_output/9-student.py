16088e6d2226dc7817cce47f435628bd5b12f681
shukufa <shukufe.memmedli2005@gmail.com>
2025-11-30 10:04:41 -0500

    python_input_output

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Return the dictionary representation of the Student instance.
+        """
+        return self.__dict__.copy()

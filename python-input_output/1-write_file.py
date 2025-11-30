805172fdb07fff641ba19a4f269d641dc9d2b7c4
shukufa <shukufe.memmedli2005@gmail.com>
2025-11-30 09:58:00 -0500

    python_input_output

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that writes a string to a text file (UTF-8)
+and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """Write text to a file and return the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)

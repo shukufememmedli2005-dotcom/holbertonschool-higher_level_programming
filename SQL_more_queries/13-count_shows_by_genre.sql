a6cae38de0e72cd189ba0b2f605daa188f094a37
shukufa <shukufa@kali.kali>
2025-11-30 09:43:06 -0500

    SQL_more_queries

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,7 @@
-SELECT g.name AS genre, COUNT(tg.show_id) AS number_of_shows
-FROM genres g
-JOIN tv_show_genres tg ON g.id = tg.genre_id
-GROUP BY g.name
+-- List all genres with the number of shows linked
+
+SELECT G.name AS genre, COUNT(SG.show_id) AS number_of_shows
+FROM tv_genres G
+JOIN tv_show_genres SG ON G.id = SG.genre_id
+GROUP BY G.name
 ORDER BY number_of_shows DESC;

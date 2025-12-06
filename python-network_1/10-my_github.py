6f4ccdb447816e560e47858e4381f6419ccd7121
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2025-12-06 16:26:30 +0000

    PYthon_network

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+username = sys.argv[1]
+token = sys.argv[2]  # This is your personal access token, not your real password
+
+url = "https://api.github.com/user"
+response = requests.get(url, auth=(username, token))
+
+if response.status_code == 200:
+    print(response.json().get('id'))
+else:
+    print(None)

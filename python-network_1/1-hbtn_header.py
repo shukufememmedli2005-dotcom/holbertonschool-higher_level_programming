6a3d0e37f9509b2fd57ed8b637d3b7374e403e02
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2025-12-06 16:13:11 +0000

    PYthon_network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module fetches a URL and prints the value of the X-Request-Id header.
+"""
+
 import urllib.request
 import sys
 
@@ -8,4 +12,3 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         headers = response.info()
         print(headers.get('X-Request-Id'))
-

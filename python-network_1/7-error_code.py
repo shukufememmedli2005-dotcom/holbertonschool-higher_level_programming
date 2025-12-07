0407b1280e42da7cc4e1df198a8091e4e1ec27d6
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2025-12-07 14:40:59 -0500

    s

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""
-Sends a request to a URL and displays the response body.
-Prints "Error code: <status>" if the HTTP status code is >= 400.
-"""
-import sys
+"""Fetches a URL and prints the body or error code if HTTP status >= 400"""
+
 import requests
+import sys
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-try:
     response = requests.get(url)
+
     if response.status_code >= 400:
-        print(f"Error code: {response.status_code}")
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
-except requests.RequestException as e:
-    print(f"Error code: {getattr(e.response, 'status_code', 'Unknown')}")

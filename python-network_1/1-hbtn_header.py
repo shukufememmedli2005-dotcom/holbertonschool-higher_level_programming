4fff333e1a5a73c68151c7c3c2cf16a4cda6346b
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2025-12-07 14:35:20 -0500

    s

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+Python script that fetches the value of X-Request-Id from a URL's response header.
+Usage: ./1-hbtn_header.py <URL>
+"""
 import sys
 from urllib import request
+
+if len(sys.argv) < 2:
+    print("Usage: ./1-hbtn_header.py <URL>")
+    sys.exit(1)
 
 url = sys.argv[1]
 

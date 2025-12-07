6cfec8c5c0340e39a519cd84e83108de85188ec8
shukufememmedli2005-dotcom <shukufe.memmedli2005@gmail.com>
2025-12-07 12:45:51 -0500

    PYthon_network

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+"""
+Sends a POST request to http://0.0.0.0:5000/search_user
+with a letter as the q parameter and prints the result.
+"""
 import sys
 import requests
 
-if __name__ == "__main__":
-    # If no argument is given, q = ""
-    q = sys.argv[1] if len(sys.argv) > 1 else ""
 
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     url = "http://0.0.0.0:5000/search_user"
     data = {"q": q}
 
     try:
-        response = requests.post(url, data=data)
-        json_data = response.json()  # Might raise ValueError
+        r = requests.post(url, data=data)
+        j = r.json()
 
-        if json_data:
-            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
+        if j:
+            print("[{}] {}".format(j.get("id"), j.get("name")))
         else:
             print("No result")
 
